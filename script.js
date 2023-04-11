fe0c2b7bd761aae9d7d1c2b54888332dd2e748f8
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-11 13:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 var nameElement = document.querySelector("#name");
 var ageElement = document.querySelector("#age");
 var btnElement = document.querySelector("#btn");
-
+let calc=()=>{
 var promise = new Promise((resolve, reject) => {
   setTimeout(() => {
     var age = ageElement.value;
     var name = nameElement.value;
-   var age = ageElement.value;
-    var name = nameElement.value;
+   // var age = ageElement.value;
+   //  var name = nameElement.value;
    if (!(age && name)) {
      alert("Please enter valid details");
-   }
     return;
+   }
     if (age < 18) {
       reject(`Oh sorry ${name}. You aren't old enough`);
     } else {
@@ -21,13 +21,19 @@ var promise = new Promise((resolve, reject) => {
     }
   }, 4000);
 });
-
-function callPromise() {
-    return promise;
+return promise;
 }
+
+// function callPromise() {
+//     return promise;
+// }
 function handleSubmit(e) {
     e.preventDefault();
-  callPromise().then(alert).catch(alert);
+  calc().then((data)=>{
+	  alert(data);
+  }).catch((data)=>{
+	  alert(data);
+  });
 }
 
 btnElement.addEventListener("click", handleSubmit);
